b839b8d7a7bbebaf0b52b003b4bb530552ee2963
prabirjoardar2010@gmail.com <prabirjoardar2010@gmail.comcls>
2019-09-25 17:46:49 +0530

    Add project files.

---
+++ Dashboard/Gruntfile.js
@@ -0,0 +1,22 @@
+﻿/// <binding ProjectOpened='ngBuild' />
+// <binding Clean='ngBuild' />
+module.exports = function (grunt) {
+
+    // Task Load
+    grunt.loadNpmTasks('grunt-shell');
+    // Project configuration.
+    
+    grunt.initConfig({
+        config: grunt.file.readJSON('appsettings.json'),
+        shell: {
+            command: ["cd <%=config.angulardir%>","ng build --deploy-url=/Ang-Dashboard/dist/Ang-Dashboard/ --watch"].join('&&')
+
+        }
+
+    });
+
+    // Default task(s).
+    grunt.registerTask('ngBuild',['shell']);
+   
+
+};
